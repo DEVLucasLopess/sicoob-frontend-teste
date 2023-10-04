99c27fb33245a411e89e2e36503803d3659b1377
Lucas Lopes <si.lucas.lopes2017@gmail.com>
2023-10-04 01:01:31 -0300

    ajudjust-front

--- src/component/CepSearch.jsx
+++ src/component/CepSearch.jsx
@@ -1,62 +1,79 @@
-import { useState } from 'react';
-import { maskCep } from '../utils/cepMask';
-import './CepSearch.css'
-import { useHttp } from '../hooks/useHttp';
+import { useState } from "react";
+import { maskCep } from "../utils/cepMask";
+import { useHttp } from "../hooks/useHttp";
+import "./CepSearch.css";
 
 function CepSearch() {
-  const [cep, setCep] = useState('');
+  const [cep, setCep] = useState("");
   const [address, setAddress] = useState(null);
   const [toastMessage, setToastMessage] = useState(null);
-  const { get } = useHttp()
+  const { get } = useHttp();
 
   const searchCep = async () => {
-    if(!cep || cep.length < 9) {
-      setToastMessage('Formato de CEP invalido');
-      return
+    if (!cep || cep.length < 9) {
+      setToastMessage("Formato de CEP invalido");
+      return;
     }
 
     try {
       const response = await get(`/address/${cep}`);
+
       setAddress(response.data);
-    } catch (error) { 
+      setToastMessage(null);
+    } catch (error) {
       if (error.response && error.response.status === 404) {
-        setToastMessage('Formato de CEP invalido');
+        setToastMessage(`${error.response?.data?.message}`);
       } else {
-        setToastMessage('Ocorreu um erro ao buscar dados.');
+        setToastMessage("Ocorreu um erro ao buscar dados.");
       }
       setAddress(null);
     }
   };
 
   return (
-    <div className='search-container'>
-      <div className='input-end-button-container'>
-        <input 
-          className='input-text'
-          value={cep} 
+    <div className="search-container">
+      <div className="input-end-button-container">
+        <input
+          className="input-text"
+          value={cep}
           onChange={(e) => {
             const maskedValue = maskCep(e.target.value);
             setCep(maskedValue);
-          }} 
-          placeholder="Digite ..." 
+          }}
+          placeholder="Digite o cep"
           maxLength={9}
           required={true}
         />
-        <button onClick={searchCep} disabled={!cep}>Pesquisar</button>
+        <button onClick={searchCep} disabled={!cep}>
+          Pesquisar
+        </button>
       </div>
 
       {address && (
         <div className="address-container">
-          <p><span className='address-info'>Logradouro:</span> {address.logradouro}</p>
-          <p><span className='address-info'>Bairro:</span> {address.bairro}</p>
-          <p><span className='address-info'>Município:</span> {address.localidade}</p>
+          <p>
+            <span className="address-info">Logradouro:</span>{" "}
+            {address.logradouro}
+          </p>
+          <p>
+            <span className="address-info">Bairro:</span> {address.bairro}
+          </p>
+          <p>
+            <span className="address-info">Municí­pio:</span>{" "}
+            {address.localidade}
+          </p>
         </div>
       )}
 
       {toastMessage && (
         <div className="toast">
-            {toastMessage}
-            <button className='button-toast-error' onClick={() => setToastMessage(null)}>X</button>
+          {toastMessage}
+          <button
+            className="button-toast-error"
+            onClick={() => setToastMessage(null)}
+          >
+            X
+          </button>
         </div>
       )}
     </div>
--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -1,11 +1,12 @@
-import axios from "axios"
-import { useCallback } from 'react'
-export const api = axios.create({baseURL: 'http://localhost:3001'})
+import axios from "axios";
+const api = axios.create({
+  baseURL: "http://localhost:3001",
+});
 
 export const useHttp = () => {
-    const get = useCallback(async ({ url }) => {
-        console.log(url)
-        return await api.get(url)
-    }, [])
-    return { get }
- }
+  const get = async (url) => {
+    return await api.get(url);
+  };
+
+  return { get };
+};
